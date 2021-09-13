a58679ecbef3a10c167fdd2eab1441b094c53ef7
willhk10 <wkeenan14@charlottesvilleschools.org>
2021-09-13 18:01:51 +0100

    help

--- Python/dice_roller.py
+++ Python/dice_roller.py
@@ -1,15 +1,21 @@
 import random
 
+print("The Dice Roller of the Century")
+
 def roll_dice():
-	roll = random.randint(1,6)
+	roll = random.randint(1,n)
 	print("You rolled a %d "  % roll)
 
-print("Automatic Dice Roller")
-val = input("Press enter to roll, or press x to quit")
-if val ==  "x":
-	print("exiting")
-	exit()
-else:
-	roll_dice()
+while True:
+	val = input("Press enter to roll, or press x to quit")
+	n = int(input("How many sides do you want on the die?"))
+	r = int(input("How many dice do you want to roll?"))
+	if val ==  "x":
+		print("exiting")
+		exit()
+	else:
+		for x in range (0,r):
+			roll_dice()
+
 
 
