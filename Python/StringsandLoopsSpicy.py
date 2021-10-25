c3a5f903a838af3799e24f884d9422af261544bf
Will Keenan <57459908+willhk10@users.noreply.github.com>
2021-10-25 19:03:48 -0400

    Update StringsandLoopsSpicy.py

--- Python/StringsandLoopsSpicy.py
+++ Python/StringsandLoopsSpicy.py
@@ -3,4 +3,5 @@
 #9.20.21
 
 [print(i) for i in input("Type in a random sentence").replace(" ", "-")+ ("-")]
-
+#This is going through each item in the inputted sentence and using the replace function to take every letter and insert a space inbetween. 
+#The for loop makes it skip a line every iteration.
