2be6c67de4be677bfaabeedaa5d07987d0f1989c
Will Keenan <57459908+willhk10@users.noreply.github.com>
2021-11-22 18:47:39 +0000

    a

--- AccelPrint.py
+++ AccelPrint.py
@@ -1,1 +1,46 @@
-aa
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+
+# Raspberry Pi pin configuration:
+RST = 24
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
+
+disp.begin()
+
+
+disp.clear()
+disp.display()
+
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw
+ a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+font = ImageFont.load_default()
+
+# Write two lines of text.
+draw.text((x, top),    'Hello',  font=font, fill=255)
+draw.text((x, top+20), 'World!', font=font, fill=255)
+
+# Display image.
+disp.image(image)
+disp.display()
+
