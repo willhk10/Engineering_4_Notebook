636ae3f703e93e82703f7df621e5b6fb6053e0e3
Will Keenan <57459908+willhk10@users.noreply.github.com>
2021-11-22 18:53:04 +0000

    a

--- AccelPrint.py
+++ AccelPrint.py
@@ -65,27 +65,7 @@ draw = ImageDraw.Draw(image)
 # Draw a black filled box to clear the image.
 draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = 2
-shape_width = 20
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = padding
-# Draw an ellipse.
-draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
-x += shape_width+padding
-# Draw a rectangle.
-draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
-x += shape_width+padding
-# Draw a triangle.
-draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
-x += shape_width+padding
-# Draw an X.
-draw.line((x, bottom, x+shape_width, top), fill=255)
-draw.line((x, top, x+shape_width, bottom), fill=255)
-x += shape_width+padding
+x = 0
 
 # Load default font.
 font = ImageFont.load_default()
