66d8f7ee507bf7acae6a8f0a09bf9f8549a96ec4
willhk10 <wkeenan14@charlottesvilleschools.org>
2021-12-09 17:21:48 +0000

    internet

--- Python/PIR.py
+++ Python/PIR.py
@@ -2,12 +2,27 @@ from gpiozero import MotionSensor
 from picamera import PiCamera
 from datetime import datetime
 
-pir = MotionSensor(4)
+pir = MotionSensor(25)
 camera = PiCamera()
 
 while True:
-    filename = "{0:%Y}-{0:%m}-{0:%d}".format(now)
-    pir.wait_for_motion()
-    camera.start_recording(filename)
-    pir.wait_for_no_motion()
-    camera.stop_recording()
+#    filename = "{0:%Y}-{0:%m}-{0:%d}".format(now)
+	filename =(f"{datetime.now()}.h264")
+#	print(datetime.now())
+	pir.wait_for_motion()
+	print("motion detected")
+	camera.start_recording(filename)
+#	print("3rd")
+	pir.wait_for_no_motion()
+	print("No motion, waitin.")
+	camera.stop_recording()
+
+#while True:
+#	if pir.motion_detected:
+#		print("motion")
+	
+#	else:
+#		print("what")
+
+#	if not pir.motion_detected:
+#		print("no motion")
